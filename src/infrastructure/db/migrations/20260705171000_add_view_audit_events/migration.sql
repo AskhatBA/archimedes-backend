2d784bae950ae3b9d2e7db6161605fd9bf1e06e1
askhat <ashat.baltabaev@300brains.com>
2026-07-06 13:27:15 +0500

    add audit logging for appointment and refunds access violations and booking conflicts

---
+++ src/infrastructure/db/migrations/20260705171000_add_view_audit_events/migration.sql
@@ -0,0 +1,13 @@
+-- AlterEnum
+-- This migration adds more than one value to an enum.
+-- With PostgreSQL versions 11 and earlier, this is not possible
+-- in a single migration. This can be worked around by creating
+-- multiple migrations, each migration adding only one value to
+-- the enum.
+
+
+ALTER TYPE "AuditEvent" ADD VALUE 'APPOINTMENT_LIST_VIEWED';
+ALTER TYPE "AuditEvent" ADD VALUE 'APPOINTMENT_VIEWED';
+ALTER TYPE "AuditEvent" ADD VALUE 'APPOINTMENT_HISTORY_VIEWED';
+ALTER TYPE "AuditEvent" ADD VALUE 'APPOINTMENT_REQUESTS_VIEWED';
+ALTER TYPE "AuditEvent" ADD VALUE 'PROFILE_VIEWED';
